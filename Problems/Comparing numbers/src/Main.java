42d15f7c7b46e325e077aa801341d3c8887aa6ae
Foxtam <foxtam7@ya.ru>
2020-09-27 16:27:55 +0300

    Generate random numbers

--- Problems/Comparing numbers/src/Main.java
+++ Problems/Comparing numbers/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
      * It returns true if at least two of three given numbers are equal, otherwise - false.
      */
     public static boolean atLeastTwoAreEqual(BigInteger num1, BigInteger num2, BigInteger num3) {
-        return (num1.equals(num2)) || (num2.equals(num3)) || (num1.equals(num3)); // fix this line
+        return num1.equals(num2) || num2.equals(num3) || num1.equals(num3); // fix this line
     }
 
     /* Do not change code below */
